b5de5606f23985c3028d20cb96274a854ab57213
metrio <dlima.nyit@gmail.com>
2020-10-11 18:41:13 -0400

    master

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|punc| punc.size < 2}.size
+    self.split(/\.|\?|\!/).delete_if {|character| character.size < 2}.size
+    # we split (. , ? , !) to form their own strings within an array, we then iterate over to find any fictional sentences such as (!!),(??),(..)
+    #if the sentence size is less then 2 characters then its not really a sentence
   end
 end
